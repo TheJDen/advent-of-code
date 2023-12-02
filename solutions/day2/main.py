09547dab53a0fdcc07326a06ea123cc5b3d0fd54
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-12-01 23:35:34 -0500

    day2 template

---
+++ solutions/day2/main.py
@@ -0,0 +1,13 @@
+def part1(f):
+    pass
+
+def part2(f):
+    pass
+
+def main():
+    with open("input.txt") as f:
+        print(part1(f))
+        # print(part2(f))
+
+if __name__ == "__main__":
+    main()
