fe586814de6ea303fa32a5f253fd8a83842b598e
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-12-02 23:52:58 -0500

    day3 template

---
+++ solutions/day3/main.py
@@ -0,0 +1,21 @@
+from itertools import accumulate
+from collections import Counter, defaultdict
+
+def process_input(f):
+    pass
+
+def part1(f):
+    pass
+
+def part2(f):
+    pass
+
+
+def main():
+    with open("input.txt") as f:
+        inputs = process_input(f)
+    print(part1(inputs))
+    # print(part1(inputs))
+
+if __name__ == "__main__":
+    main()
