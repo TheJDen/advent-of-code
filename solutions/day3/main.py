27423aa1604547de27539a0759822c559fe707aa
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-12-03 00:23:12 -0500

    cleaned up a little

--- solutions/day3/main.py
+++ solutions/day3/main.py
@@ -14,13 +14,30 @@ def part(lines, i, j):
         end += 1
     return (i, start, end)
 
-def process_input(lines):
-    #parts = set()
+def get_unique_parts(lines):
+    parts = set()
+    for i, row in enumerate(lines):
+        for j, char in enumerate(row):
+            close_parts = set()
+            if char in NOT_SYMBOL:
+                continue
+            for di, dj in ADJ:
+                if not 0 <= i + di < len(row):
+                    continue
+                if not 0 <= j + dj < len(row):
+                    continue
+                if lines[i + di][j + dj] not in string.digits:
+                    continue
+                close_parts.add(part(lines, i + di, j + dj))
+            parts.update(close_parts)
+    return [lines[i][start: end] for i, start, end in parts]
+    
+
+def get_gear_parts(lines):
     gears = []
     for i, row in enumerate(lines):
         for j, char in enumerate(row):
             close_parts = set()
-            #if char in NOT_SYMBOL:
             if char != '*':
                 continue
             for di, dj in ADJ:
@@ -33,10 +50,8 @@ def process_input(lines):
                 close_parts.add(part(lines, i + di, j + dj))
             if len(close_parts) == 2:
                     gears.append(close_parts)
-            #parts.update(close_parts)
     return [[lines[i][start: end] for i, start, end in parts] for parts in gears]
     
-
 def part1(part_num_strs):
     return sum(int(part_num_str) for part_num_str in part_num_strs)
 
@@ -46,10 +61,9 @@ def part2(gears):
 
 def main():
     with open("input.txt") as f:
-        inputs = process_input([line.rstrip() for line in f])
-        print(inputs)
-    #print(part1(inputs))
-    print(part2(inputs))
+        lines = [line.rstrip() for line in f]
+    print(part1(get_unique_parts(lines)))
+    print(part2(get_gear_parts(lines)))
 
 if __name__ == "__main__":
     main()
