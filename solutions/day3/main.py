88bc50973f61090ce4db62cf7da52aaa1538e3cc
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-12-03 20:29:27 -0500

    pulled out part filter, removed redundant gear function

--- solutions/day3/main.py
+++ solutions/day3/main.py
@@ -11,7 +11,7 @@ def part(lines, i, j):
         end += 1
     return (i, start, end)
 
-def get_unique_parts(lines, is_symbol: callable):
+def get_unique_parts(lines, is_symbol: callable, should_add_parts: callable = lambda parts: True ):
     parts = []
     for i, row in enumerate(lines):
         for j, char in enumerate(row):
@@ -26,28 +26,9 @@ def get_unique_parts(lines, is_symbol: callable):
                 if lines[i + di][j + dj] not in string.digits:
                     continue
                 close_parts.add(part(lines, i + di, j + dj))
-            parts.append(close_parts)
+            if should_add_parts(close_parts):
+                parts.append(close_parts)
     return [[lines[i][start: end] for i, start, end in part] for part in parts]
-    
-
-def get_gear_parts(lines, is_symbol: callable):
-    gears = []
-    for i, row in enumerate(lines):
-        for j, char in enumerate(row):
-            close_parts = set()
-            if not is_symbol(char):
-                continue
-            for di, dj in ADJ:
-                if not 0 <= i + di < len(row):
-                    continue
-                if not 0 <= j + dj < len(row):
-                    continue
-                if lines[i + di][j + dj] not in string.digits:
-                    continue
-                close_parts.add(part(lines, i + di, j + dj))
-            if len(close_parts) == 2:
-                    gears.append(close_parts)
-    return [[lines[i][start: end] for i, start, end in parts] for parts in gears]
     
 def part1(parts):
     return sum(int(part_num_str) for part in parts for part_num_str in part)
@@ -55,12 +36,12 @@ def part1(parts):
 def part2(gears):
     return sum(int(part1) * int(part2) for part1, part2 in gears)
 
-
 def main():
     with open("input.txt") as f:
         lines = [line.rstrip() for line in f]
     print(part1(get_unique_parts(lines, lambda char: char not in NOT_SYMBOL)))
-    print(part2(get_gear_parts(lines, lambda char: char == '*')))
+    print(part2(get_unique_parts(lines, lambda char: char == '*', lambda parts: len(parts) == 2)))
+
 
 if __name__ == "__main__":
     main()
