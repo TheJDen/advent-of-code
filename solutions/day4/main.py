5736fb212260a536a117f50a9947929773d3185d
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-12-04 17:05:57 -0500

    bottom up

--- solutions/day4/main.py
+++ solutions/day4/main.py
@@ -16,15 +16,23 @@ def part2(cards):
 
     return sum(total_cards(i) for i in range(len(cards)))
 
+def part2_bottom_up(cards):
+    total_cards = [0] * len(cards)
+    for i in reversed(range(len(cards))):
+        want_nums, have_nums = cards[i]
+        num_matching = sum(want_num in have_nums for want_num in want_nums)
+        total_cards[i] = 1 + sum(total_cards[i + j] for j in range(1, num_matching + 1))
+    return sum(total_cards)
+
 def main():
     with open("input.txt") as f:
         cards = []
         for line in f:
             title, remaining_card = line.rstrip().split(':')
             cards.append([section.strip().split() for section in remaining_card.rstrip().split('|')] )
-        print(cards[-1])
     #print(part1(cards))
-    print(part2(cards))
+    #print(part2(cards))
+    print(part2_bottom_up(cards))
 
 
 if __name__ == "__main__":
