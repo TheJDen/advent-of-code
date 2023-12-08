f0336c54d5b37c3c25e5c42e3feacb6ebf4b4740
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-12-07 21:13:48 -0500

    template typo

--- solutions/day6/main.py
+++ solutions/day6/main.py
@@ -7,7 +7,7 @@ def part1(times, distances):
         num_ways *= ways
     return num_ways
 
-def part1(times, distances):
+def part2(times, distances):
     race_time = int("".join(times))
     record_distance = int("".join(distances))
     return sum((race_time - i) * i > record_distance for i in range(1, race_time))
@@ -21,7 +21,7 @@ def main():
     with open("input.txt") as f:
         times, distances = parse_input([line.rstrip() for line in f])
     print(part1(times, distances))
-    #print(part2(cards))
+    print(part2(times, distances))
 
 
 if __name__ == "__main__":
