a37ebb3ff79ce54b7424cf214f28d16361d6530f
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-12-07 22:38:23 -0500

    improvement from dajeff

--- solutions/day7/main.py
+++ solutions/day7/main.py
@@ -32,31 +32,8 @@ def part1(hands, bids):
     return sum(rank * int(bids[i]) for rank, i in enumerate(sorted(range(len(hands)), key=lambda i: (get_hand_type_p1(hands[i]), get_values(hands[i], "AKQJT98765432"))), start=1))
 
 def get_hand_type_p2(hand):
-    symbol_counts = Counter(hand)
-    symbol_count_counts = Counter(symbol_counts.values())
-    if symbol_counts['J'] == 0:
-        return get_hand_type_p1(hand)
-    if symbol_counts['J'] == 1:
-        if symbol_count_counts[4] == 1:
-            return FIVE_OF_A_KIND
-        if symbol_count_counts[3] == 1:
-            return FOUR_OF_A_KIND
-        if symbol_count_counts[2] == 2:
-            return FULL_HOUSE
-        if symbol_count_counts[2] == 1:
-            return THREE_OF_A_KIND
-        return ONE_PAIR
-    if symbol_counts['J'] == 2:
-        if symbol_count_counts[3] == 1:
-            return FIVE_OF_A_KIND
-        if symbol_count_counts[2] > 1:
-            return FOUR_OF_A_KIND
-        return THREE_OF_A_KIND
-    if symbol_counts['J'] == 3:
-        if symbol_count_counts[2] == 1:
-            return FIVE_OF_A_KIND
-        return FOUR_OF_A_KIND
-    return FIVE_OF_A_KIND
+    return get_hand_type_p1(hand.replace('J', max("AKQT98765432", key=lambda char: hand.count(char))))
+ 
 
 def part2(hands, bids):
     return sum(rank * int(bids[i]) for rank, i in enumerate(sorted(range(len(hands)), key=lambda i: (get_hand_type_p2(hands[i]), get_values(hands[i], "AKQT98765432J"))), start=1))
